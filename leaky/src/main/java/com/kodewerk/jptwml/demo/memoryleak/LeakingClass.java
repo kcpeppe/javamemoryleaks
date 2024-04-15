9d230c25f206438b01087200f0e4c236ba649ff3
Dr Heinz M. Kabutz <heinz@javaspecialists.eu>
2024-04-15 21:22:53 +0300

    Upgraded Leaky to use Swing instead of obsoleted JavaFX

--- leaky/src/main/java/com/kodewerk/jptwml/demo/memoryleak/LeakingClass.java
+++ leaky/src/main/java/com/kodewerk/jptwml/demo/memoryleak/LeakingClass.java
@@ -5,13 +5,12 @@ package com.kodewerk.jptwml.demo.memoryleak;
  * All right reserved
  ********************************************/
 
-import java.util.HashMap;
+import java.util.*;
 
 public class LeakingClass {
-
-    private HashMap<String,String> alongForTheRide = new HashMap();
-    private String alsoAlongForTheRide;
-    private LeakingField leakingField;
+    private final Map<String, String> alongForTheRide = new HashMap<>();
+    private final String alsoAlongForTheRide;
+    private final LeakingField leakingField;
 
     public LeakingClass(String parameter, LeakingField field) {
         alsoAlongForTheRide = parameter;
@@ -19,7 +18,7 @@ public class LeakingClass {
     }
 
     public String toString() {
-        StringBuffer buffer = new StringBuffer(alongForTheRide.toString());
+        var buffer = new StringBuffer(alongForTheRide.toString());
         buffer.append(alsoAlongForTheRide);
         buffer.append(leakingField);
         return buffer.toString();
--- leaky/src/main/java/com/kodewerk/jptwml/demo/memoryleak/Leaky.java
+++ leaky/src/main/java/com/kodewerk/jptwml/demo/memoryleak/Leaky.java
@@ -5,54 +5,76 @@ package com.kodewerk.jptwml.demo.memoryleak;
  * All right reserved
  ********************************************/
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import javafx.util.Duration;
-
+import javax.swing.*;
+import java.awt.*;
 import java.lang.management.ManagementFactory;
+import java.util.concurrent.*;
 
-public class Leaky extends Application {
+public class Leaky extends JFrame {
+    private final static ExecutorService leakerService =
+            Executors.newSingleThreadExecutor();
 
-    private LeakyModel model;
-    private TextField heapOccupancyTextField;
+    public Leaky() {
+        super("Leaky");
 
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("Leaky");
-        model = new LeakyModel();
+        var model = new LeakyModel();
 
-        TextField integerField = new TextField("1000000");
+        var numberOfObjectsField = new JTextField("1000000", 10);
+        var button = new JButton("Do Stuff");
+        button.addActionListener(e -> {
+            button.setEnabled(false);
+            numberOfObjectsField.setEnabled(false);
+            leakerService.submit(() -> {
+                try {
+                    model.leak(Long.parseLong(numberOfObjectsField.getText()));
+                    EventQueue.invokeLater(() -> {
+                        button.setEnabled(true);
+                        numberOfObjectsField.setEnabled(true);
+                        numberOfObjectsField.requestFocus();
+                    });
+                } catch (Throwable t) {
+                    t.printStackTrace();
+                }
+            });
+        });
+        button.setDefaultCapable(true);
+        getRootPane().setDefaultButton(button);
 
-        Button button = new Button("Do Stuff");
-        button.setOnAction((event)-> model.leak(Integer.valueOf(integerField.getText())));
+        var controls = new JPanel();
+        controls.add(numberOfObjectsField);
+        controls.add(button);
 
-        HBox controls = new HBox(5, integerField, button);
+        var heapOccupancyTextField = new JTextField("0", 15);
+        heapOccupancyTextField.setEditable(false);
+        JPanel views = new JPanel();
+        views.add(new JLabel("Heap Occupancy (MB):"));
+        views.add(heapOccupancyTextField);
 
-        heapOccupancyTextField = new TextField("0");
-        HBox views = new HBox( 5, new Label("Heap Occupancy (KB)", heapOccupancyTextField));
-        VBox root = new VBox(5, views, controls);
-        root.setAlignment(Pos.CENTER);
-        Scene scene = new Scene(root, 450, 90);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        var root = new JPanel();
+        root.setLayout(new BoxLayout(root, BoxLayout.Y_AXIS));
+        root.add(views);
+        root.add(controls);
 
-        Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(
-                new KeyFrame(Duration.millis(1000), (ActionEvent actionEvent) ->
-                        heapOccupancyTextField.setText(Long.toString(
-                                ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getUsed() / 1024))));
-        timeline.setCycleCount(1000);
-        timeline.setAutoReverse(true);  //!?
-        timeline.play();
+        add(root);
+
+        new Timer(1000, e -> {
+            var heapOccupancy = ManagementFactory.getMemoryMXBean()
+                    .getHeapMemoryUsage()
+                    .getUsed() / (1024 * 1024);
+            heapOccupancyTextField.setText("" + heapOccupancy);
+        }).start();
+    }
+
+    public static void main(String... args) {
+        System.out.println("Turning on Verbose GC");
+        ManagementFactory.getMemoryMXBean().setVerbose(true);
+        EventQueue.invokeLater(() -> {
+            Leaky leaky = new Leaky();
+            leaky.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            leaky.setSize(450, 120);
+            leaky.setLocationRelativeTo(null);
+            leaky.setVisible(true);
+        });
     }
 }
+
